5bfbcd0306ba2b7657e0d65026641107facc5d67
Rajdeep De <surjode2003@gmail.com>
2023-12-02 12:13:43 +0530

    added required assets

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,18 +25,43 @@ const locationSequence = [
 
 const Hero = () => {
   return (
-    <section className="h-[80vh] xl:h-[850px]" id="home">
-        <div className="container mx-auto h-full flex justify-center
+    <section className="h-[80vh] xl:h-[850px] mb-10" id="home">
+        <div className="container mx-auto h-full flex justify-center 
         items-center xl:justify-start">
             {/* text */}
-            <div>text</div>
+            <div className=" h-full flex flex-col justify-center items-center xl:items-start z-20 pt-12">
+              <MouseParallaxContainer
+               globalFactorX={0.1} 
+               globalFactorY={0.2} 
+               resetOnLeave 
+               className="relative flex items-center h-[120px] xl:h-max xl:w-[840px]">
+                <MouseParallaxChild
+                 factorX={0.2}
+                 factorY={0.4}
+                 className="relative ">
+                  <motion.div
+                  variants={fadeIn('up',0.4)}
+                  initial='hidden'
+                  whileInView={'show'}
+                  viewport={{once:false, amount:0.3}}
+                   className="w-[300px] h-[101.37px] xl:w-[725px] xl:h-[244.97px]">
+                    <Image src={'/assets/hero/TAYLOR.svg'}
+                    fill
+                    alt=''
+                    className="object-contain"/>
+                  </motion.div>
+                </MouseParallaxChild>
+              </MouseParallaxContainer>
+            </div>
             {/* image */}
-            <div>
-                <Image src={'/assets/hero/singer.png'}
+            <div className="hidden xl:flex absolute right-0 top-0 before:w-[784px] before:h-[893px]
+             before:absolute before:right-0 before:top-0 before:bg-singerOverlay before:z-10">
+                <Image src={'/assets/hero/mm 1.png'}
                  width={617}
                  height={893}
                  alt=''
-                 quality={50}
+                 quality={100}
+                 
                  priority />
             </div>
         </div>
